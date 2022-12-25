f3dae05403153ca309189eb26ea8d4cce701c342
Ali Taha <aliestaha04@gmail.com>
2022-12-24 22:11:13 -0500

    Initial commit

---
+++ src/components/profileSide/ProfileSide.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import LogoSearch from './LogoSearch/LogoSearch'
+import ProfileCard from './ProfileCard/ProfileCard'
+
+const ProfileSide = () => {
+  return (
+    <div className="ProfileSide">
+        <LogoSearch/>
+        <ProfileCard/>
+    </div> 
+
+  )
+}
+
+export default ProfileSide
