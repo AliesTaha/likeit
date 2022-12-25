11a52a5237a3c4c3d22cfbb3a8ee1c06faf2e93f
Ali Taha <aliestaha04@gmail.com>
2022-12-25 02:54:54 -0500

    Trends implemented
    Right side of the website, where the trends and everything do show

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import ProfileSide from '../../components/profileSide/ProfileSide'
 import './Home.css'
 import PostSide from '../../components/PostSide/PostSide'
+import RightSide from '../../components/RightSide/RightSide'
 
 const Home = () => {
   return (
     <div className='Home'>
         <ProfileSide/>
         <PostSide/>
-        <div className='RightSide'>Rightside</div>
+        <RightSide/>
     </div>
   )
 }
