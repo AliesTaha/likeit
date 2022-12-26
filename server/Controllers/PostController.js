423b7ed6102f8e7ba74b9deb101fce43ee6a3afa
Ali Taha <aliestaha04@gmail.com>
2022-12-26 00:50:20 -0500

    Final backedn
    b

--- server/Controllers/PostController.js
+++ server/Controllers/PostController.js
@@ -2,6 +2,7 @@ import PostModel from "../Models/postModel.js";
 import mongoose from "mongoose";
 import UserModel from "../Models/userModel.js";
 
+// Create new Post
 export const createPost = async (req, res) => {
   const newPost = new PostModel(req.body);
 
@@ -58,11 +59,11 @@ export const deletePost = async (req, res) => {
       res.status(403).json("Action forbidden...not your post o-O");
     }
   } catch (error) {
-    res.status(500).json(error);
+    res.status(500).json(error); 
   }
 };
 
-// like/dislike a post
+// Like and remove like of a post
 export const likePost = async (req, res) => {
   const id = req.params.id;
   const { userId } = req.body;
@@ -83,27 +84,43 @@ export const likePost = async (req, res) => {
   }
 };
 
-// Get Timeline POsts
+// Get Timeline Posts (show posts of followers)
+//Show posts of the person himself
 export const getTimelinePosts = async (req, res) => {
   const userId = req.params.id;
 
   try {
+
+    //Receive post of current user, returning posts of user
     const currentUserPosts = await PostModel.find({ userId: userId });
+    
+    //Returning posts of user following
     const followingPosts = await UserModel.aggregate([
       {
+        //I don't really understand aggregate but like
+        //Match query gives a single document contains id
         $match: {
           _id: new mongoose.Types.ObjectId(userId),
         },
       },
+      //Lookup will match the document, place query on user model
+      //Get result from post model
+      //Aggregate pipeline compares it from user to post model
       {
         $lookup: {
+
+          //Integrate with post model
           from: "posts",
+
+          //local field has array of user ids of following, so im matching them
           localField: "following",
           foreignField: "userId",
+          //result as following posts
           as: "followingPosts",
         },
       },
       {
+        //which field you want to return from the pipeline
         $project: {
           followingPosts: 1,
           _id: 0,
@@ -113,9 +130,13 @@ export const getTimelinePosts = async (req, res) => {
 
     res
       .status(200)
+      //need to spread the object...outside the array...spread it
       .json(currentUserPosts.concat(...followingPosts[0].followingPosts)
+      
+      //sort according to time, as it should be
       .sort((a,b)=>{
-          return b.createdAt - a.createdAt;
+          return b.createdAt - a.createdAt; //this gives latest post first
+          //return b then return a 
       })
       );
   } catch (error) {
