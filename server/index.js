b2e3876b074d090cb0dd20466e42f87a3f015597
Ali Taha <aliestaha04@gmail.com>
2022-12-26 05:04:13 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from 'cors'
 import AuthRoute from './Routes/AuthRoute.js'
 import UserRoute from './Routes/UserRoute.js'
 import PostRoute from './Routes/PostRoute.js'
@@ -14,6 +15,10 @@ const app = express();
 // Middleware
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+
+//Configure app to use cors
+app.use(cors())
 
 dotenv.config();
 
