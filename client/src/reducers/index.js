3d3db077c033d72a8eb683a7a86d2055ad0a99e0
Ali Taha <aliestaha04@gmail.com>
2022-12-27 04:54:36 -0500

    Final Site
    Failed at chat attemp

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,6 +2,5 @@ import { combineReducers } from "redux";
 
 import authReducer from "./AuthReducer";
 import postReducer from "./PostReducer";
-import chatReducer from "./ChatUserReducer";
 
-export const reducers = combineReducers({authReducer,postReducer, chatReducer})
+export const reducers = combineReducers({authReducer,postReducer})
